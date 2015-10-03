e27fccd15be4d8acf45db79af4e282fb4f1cff9d
uotbw <info@hgamer3d.org>
2015-10-03 23:34:14 +0200

    fix .travis.yaml

--- Scripts/building/installHaskell.sh
+++ Scripts/building/installHaskell.sh
@@ -9,7 +9,7 @@ set -e
 wget -q -O- https://s3.amazonaws.com/download.fpcomplete.com/ubuntu/fpco.key | apt-key add -
 echo 'deb http://download.fpcomplete.com/ubuntu/vivid stable main'| tee /etc/apt/sources.list.d/fpco.list
 apt-get update 
-apt-get install libtinfo -y
+apt-get install libtinfo-dev -y
 apt-get install stack -y
 stack setup --resolver lts-3.4
 
